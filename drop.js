7787a1a9fc329ff5f24425bb7152ef31a4eea189
lovelytong <tongyufujing@qq.com>
2018-07-04 12:00:18 +0800

    'first'

---
+++ drop.js
@@ -0,0 +1,20 @@
+
+function drop(v, d, a) {
+    let ball = document.getElementById("ball")
+    v = v * a;
+    let top = ball.offsetTop;
+
+    if (top > 600) {
+        d = -1;
+        a = 0.8;
+    }
+    if (v < 2 ) {
+        d = 1;
+        a = 1.1;
+    }
+    top += v * d;
+    ball.style.top = top + "px";
+    setTimeout(() => {
+        drop(v, d, a)
+    }, 50)
+}
